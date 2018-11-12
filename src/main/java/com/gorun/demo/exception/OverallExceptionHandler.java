36e9af61634718e7a897f1933c5ea4ecff561d8b
gao1996 <1187827494@qq.com>
2018-11-12 22:20:25 +0800

    update

---
+++ src/main/java/com/gorun/demo/exception/OverallExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.gorun.demo.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@ControllerAdvice
+public class OverallExceptionHandler {
+    public static final String ERROR_VIEW = "error";
+    @ExceptionHandler(value=Exception.class)
+    public Object errorHandler(HttpServletRequest request,
+                               HttpServletResponse response, Exception e) throws Exception {
+        e.printStackTrace();
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("exception", e);
+        modelAndView.addObject("url", request.getRequestURL());
+        modelAndView.setViewName(ERROR_VIEW);
+        return modelAndView;
+
+    }
+}
