e708071e4424450d0ed2389d869718d09945a813
gao1996 <1187827494@qq.com>
2018-11-18 03:02:41 +0800

    增加redis缓存

--- src/main/java/com/gorun/demo/SpringbootApplication.java
+++ src/main/java/com/gorun/demo/SpringbootApplication.java
@@ -1,9 +1,12 @@
 package com.gorun.demo;
 
+import com.gorun.demo.base.BaseController;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @SpringBootApplication
 //开启定时任务
@@ -11,8 +14,8 @@ import org.springframework.cache.annotation.EnableCaching;
 //开启缓存
 @EnableCaching
 @MapperScan("com.gorun.demo.mapper")
+public class SpringbootApplication extends BaseController {
 
-public class SpringbootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
