2cf07030cdbe340ba286203269480f210126b29f
gao1996 <1187827494@qq.com>
2018-11-18 04:31:42 +0800

    在service层实现redis

--- src/main/java/com/gorun/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/gorun/demo/service/impl/UserServiceImpl.java
@@ -3,9 +3,11 @@ package com.gorun.demo.service.impl;
 import com.gorun.demo.mapper.UserMapper;
 import com.gorun.demo.model.User;
 import com.gorun.demo.service.UserService;
+import net.sf.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,9 +16,17 @@ import java.util.List;
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserMapper userMapper;
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+    String result;
 
     public List<User> allData() {
-        return userMapper.selectByExample(null);
+        if (this.result == null) {
+            stringRedisTemplate.opsForValue().set("user",
+                    JSONArray.fromObject(userMapper.selectByExample(null)).toString());
+        }
+        this.result = stringRedisTemplate.opsForValue().get("user");
+        return JSONArray.fromObject(this.result);
     }
 
     @Override
